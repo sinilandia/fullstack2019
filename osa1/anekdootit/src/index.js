363dff10da405ae433d1dd9e7c5d245d076e909d
sinilandia <aisinilandia@gmail.com>
2019-01-28 21:14:22 +0200

    lisää tehtävä 1.13, mutta ääntenlasku ei toimi taulukolla

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -8,8 +8,8 @@ const Button = (props) => (
 )
 
 const generateRandomNumber = (value) => {
-    let random = Math.random()*6
-    
+    let random = Math.random() * anecdotes.length
+
     //en ihan ymmärrä, miksi välillä "next anecdote"-buttonia painaessa 
     //sivusto ei näytä uutta anekdoottia.
     //luulin tämän johtuvan siitä, että satunnaisgeneraattori
@@ -18,7 +18,9 @@ const generateRandomNumber = (value) => {
     //satunnaislukugeneraattori arpoo uuden luvun
     //mutta tätä ei-uuden-anekdootin-näyttämistä tapahtuu edelleen!
     //ehkä netti pätkii? vai oliko tämä nyt sellainen tilanne, ettei if:iä saa käyttää?
-    if (random===value) {
+    //vai onko laskurini väärin - tilaa ei saisi suoraan muuttaa setToSelectedissa alla?
+
+    if (random === value) {
         return (
             generateRandomNumber(value)
         )
@@ -28,32 +30,50 @@ const generateRandomNumber = (value) => {
     )
 }
 
-
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
-
-  const setToSelected = newValue => {
-    setSelected(newValue)
+const addVotes = props => {
+    const copy = [...props.votes]
+    console.log([copy])
+    console.log([props.votes])
+    copy[props.selected] = +1
+    return (
+        copy
+    )
 }
 
-  return (
-    <div>
-      <p>{props.anecdotes[selected]}</p>
-      <Button handleClick={() => setToSelected(generateRandomNumber(selected))} text="next anecdote" />
-    </div>
-  )
+
+
+const App = (props) => {
+    const [selected, setSelected] = useState(0)
+    const votes = new Array(anecdotes.length).fill(0)
+
+    const setToSelected = newValue => {
+        setSelected(newValue)
+    }
+
+    const setToVotes = newArray => {
+        votes = newArray.map(value => value)
+    }
+
+
+    return (
+        <div>
+            <p>{props.anecdotes[selected]}</p>
+            <Button handleClick={() => setToSelected(generateRandomNumber(selected))} text="next anecdote" />
+            <Button handleClick={() => setToVotes(addVotes(props.votes={votes}, props.selected={selected}))} text="vote" />
+        </div>
+    )
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
+    <App anecdotes={anecdotes} />,
+    document.getElementById('root')
 )
